b27f24fdb315755240b4845ca89348e97900f7f4
cjax44 <cjax44@gmail.com>
2019-04-17 08:43:11 -0400

    updated bank hw

--- bankexample.sql
+++ bankexample.sql
@@ -15,3 +15,13 @@ CREATE TABLE Transactions (
     txn_type nvarchar(50) not null,
     account_ID int not null
 );
+
+Insert Accounts (account_holder, balance, fees) values ('Anna Bengel', 888800, 25000)
+Insert Accounts (account_holder, balance, fees) values ('Chase Williams', 544400, 217500)
+Insert Accounts (account_holder, balance, fees) values ('Jonelle Wehrman', 333300, 8800)
+Insert Accounts (account_holder, balance, fees) values ('Steven Ross', 332200, 25000)
+
+Insert Transactions (amount, txn_type, account_ID) values (50000, 'Deposit', 3)
+Insert Transactions (amount, txn_type, account_ID) values (-20000, 'Withdrawal', 34)
+Insert Transactions (amount, txn_type, account_ID) values (20000, 'Deposit', 4)
+Insert Transactions (amount, txn_type, account_ID) values (24800, 'Deposit', 1)

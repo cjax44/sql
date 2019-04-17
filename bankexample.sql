0bd1582b35ae48e624ecd59a2a1ef770b310ad7d
cjax44 <cjax44@gmail.com>
2019-04-17 08:43:59 -0400

    updated hw final revision

--- bankexample.sql
+++ bankexample.sql
@@ -16,12 +16,12 @@ CREATE TABLE Transactions (
     account_ID int not null
 );
 
-Insert Accounts (account_holder, balance, fees) values ('Anna Bengel', 888800, 25000)
-Insert Accounts (account_holder, balance, fees) values ('Chase Williams', 544400, 217500)
-Insert Accounts (account_holder, balance, fees) values ('Jonelle Wehrman', 333300, 8800)
-Insert Accounts (account_holder, balance, fees) values ('Steven Ross', 332200, 25000)
+Insert Accounts (account_holder, balance, fees) values ('Anna Bengel', 888800, 25000);
+Insert Accounts (account_holder, balance, fees) values ('Chase Williams', 544400, 217500);
+Insert Accounts (account_holder, balance, fees) values ('Jonelle Wehrman', 333300, 8800);
+Insert Accounts (account_holder, balance, fees) values ('Steven Ross', 332200, 25000);
 
-Insert Transactions (amount, txn_type, account_ID) values (50000, 'Deposit', 3)
-Insert Transactions (amount, txn_type, account_ID) values (-20000, 'Withdrawal', 34)
-Insert Transactions (amount, txn_type, account_ID) values (20000, 'Deposit', 4)
-Insert Transactions (amount, txn_type, account_ID) values (24800, 'Deposit', 1)
+Insert Transactions (amount, txn_type, account_ID) values (50000, 'Deposit', 3);
+Insert Transactions (amount, txn_type, account_ID) values (-20000, 'Withdrawal', 34);
+Insert Transactions (amount, txn_type, account_ID) values (20000, 'Deposit', 4);
+Insert Transactions (amount, txn_type, account_ID) values (24800, 'Deposit', 1);
